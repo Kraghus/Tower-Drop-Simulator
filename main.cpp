d1666094411aadddd241a807da325f4cbdb60291
Kraghus <masongage@outlook.com>
2022-05-20 09:31:45 -0400

    Added input function for tower height

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+double getTowerHeight()
+{
+	std::cout << "Enter the height of the tower in meters: ";
+	double towerHeight{};
+	std::cin >> towerHeight;
+	return towerHeight;
+}
+
 int main()
 {
+	double towerHeight{ getTowerHeight() };
+
+
 	return 0;
 }
