7e59775da30275ef7aef92f7d2017a17e44aa32c
Kraghus <masongage@outlook.com>
2022-05-21 10:55:07 -0400

    Added io
    Splitting files from main.

--- main.cpp
+++ main.cpp
@@ -1,29 +1,8 @@
 #include <iostream>
 
-double getTowerHeight()
-{
-	std::cout << "Enter the height of the tower in meters: ";
-	double towerHeight{};
-	std::cin >> towerHeight;
-	return towerHeight;
-}
-
-double distanceFallen(double towerHeight, double seconds)
-{
-	constexpr double gravity { 9.8 };
-	double distanceFallen{ (gravity * (seconds * seconds) / 2) };
-	return distanceFallen;
-}
-
-void printHeight(double towerHeight, double seconds, double distanceFallen)
-{
-	if (towerHeight - distanceFallen <= 0)
-		std::cout << "At " << seconds << " second(s), the object is on the ground.\n";
-	else
-		std::cout << "At " << seconds << " second(s), the object is at height: " <<
-		             (towerHeight - distanceFallen) << " meters.\n";
-
-}
+double getTowerHeight();
+double distanceFallen(double towerHeight, double seconds);
+void printHeight(double towerHeight, double seconds, double distanceFallen);
 
 int main()
 {
